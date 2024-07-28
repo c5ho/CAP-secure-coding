66bb02281bf654a410536dc12e573e780c62325f
Calvin Ho <91750837+c5ho@users.noreply.github.com>
2024-07-28 19:30:12 -0400

    Create simLab2Ex1.c

---
+++ simLab2Ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 256
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    fprint(stderr, "Please provide the address of a file as an input. \n");
+    return -1;
+  }
+  char cmd[BUFSIZE] = "wc -c < ";
+  strcat(cmd, argv[1]);
+  system(cmd);
+}
